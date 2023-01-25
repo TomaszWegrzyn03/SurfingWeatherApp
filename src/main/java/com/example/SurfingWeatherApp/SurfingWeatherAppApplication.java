c76d2a888226148c2ed62acd2b40366ffddf4791
TomaszWegrzyn03 <tomaszwegrzyn.contact@gmail.com>
2023-01-25 18:13:50 +0100

    Initial app configuration.

---
+++ src/main/java/com/example/SurfingWeatherApp/SurfingWeatherAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.SurfingWeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SurfingWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfingWeatherAppApplication.class, args);
+	}}
+
+
+
+
+
+
---
+++ src/test/java/com/example/SurfingWeatherApp/SurfingWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SurfingWeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfingWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
